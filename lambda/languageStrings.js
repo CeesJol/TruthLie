0e17943edcaf3afde249ff50c8684909ae473c65
Cees <cjol99@icloud.com>
2020-09-21 16:27:58 +0200

    Update language strings

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -1,20 +1,18 @@
 module.exports = {
   translation: {
-    SKILL_NAME: 'High Low Game',
+    SKILL_NAME: 'Two Truths and a Lie Game',
     EXIT_MESSAGE: 'Thanks for playing!',
-    FALLBACK_MESSAGE_DURING_GAME: `I can't help you with that.  Try guessing a number between 0 and 100. `,
-    FALLBACK_REPROMPT_DURING_GAME: 'Please guess a number between 0 and 100.',
-    FALLBACK_MESSAGE_OUTSIDE_GAME: `I can't help you with that.  I will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?`,
-    GUESS_CORRECT_MESSAGE: '%s is correct! Would you like to play again?',
-    LAUNCH_MESSAGE: 'Welcome to High Low guessing game. You have played %s times. Would you like to play?',
-    TOO_HIGH_MESSAGE: '%s is too high.',
-    TOO_HIGH_REPROMPT: 'Try guessing a smaller number.',
-    TOO_LOW_MESSAGE: '%s is too low.',
-    TOO_LOW_REPROMPT: 'Try guessing a larger number.',
+    FALLBACK_MESSAGE_DURING_GAME: `I can't help you with that. Do you want to play a game?`,
+    FALLBACK_REPROMPT_DURING_GAME: 'Please say whether you would like to play a game.',
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `I can't help you with that.  I will tell two truths and  a lie and you try to tell which one is the lie. Would you like to play?`,
+    GUESS_CORRECT_MESSAGE: 'Statement %s is indeed a lie! Would you like to play again?',
+    LAUNCH_MESSAGE: 'Welcome to Two Truths and a Lie. You have played %s times. Would you like to play?',
+    INCORRECT_MESSAGE: '%s is not a lie. TODO, I should say which one actually was a lie.',
+    INCORRECT_REPROMPT: '%s is not a lie. TODO, I should say which one actually was a lie.',
     CONTINUE_MESSAGE: 'Say yes to play or no to quit.',
-    HELP_MESSAGE: 'I am thinking of a number between zero and one hundred, try to guess it and I will tell you if it is higher or lower.',
-    HELP_REPROMPT: 'Try saying a number.',
+    HELP_MESSAGE: 'I will tell you two truths and a lie, you tell me which one is a lie and I will tell you if you were right.',
+    HELP_REPROMPT: 'Say yes to play.',
     ERROR_MESSAGE: 'Sorry, an error occurred.',
-    YES_MESSAGE: 'Great! Try saying a number to start the game.'
+    YES_MESSAGE: 'Great! The statements are:'
   }
 };
